63fb6a44dca96f2c5684bfd1ab42936be6d288cd
omkarkarale <82709945+omkarkarale@users.noreply.github.com>
2021-05-06 18:21:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(800,400);
+  fixedRect=createSprite(200, 200, 50, 80); 
+  movingRect=createSprite(400, 200, 80, 50); 
+}
+
+function draw() {
+  background(255,255,255);  
+
+  fixedRect.shapeColor="green"; 
+  movingRect.shapeColor="green";
+
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+  // console.log()
+
+  if((movingRect.x-fixedRect.x < movingRect.width/2+fixedRect.width/2) 
+  && (movingRect.y-fixedRect.y < movingRect.height/2+fixedRect.height/2)  
+  && (fixedRect.y-movingRect.y < movingRect.height/2+fixedRect.height/2) 
+  && (fixedRect.x-movingRect.x < movingRect.width/2+fixedRect.width/2)) 
+  { 
+    fixedRect.shapeColor="red"; 
+    movingRect.shapeColor="red";
+  }
+
+  drawSprites();
+}
